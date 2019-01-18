bc070612e1cae133f90744d2fa43374286a2072e
swc_20180045 <ty0207@sjtu.edu.cn>
2019-01-18 11:26:48 +0800

    change the settings to ignore the foreignkey check

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES',foreign_key_checks = 0;",
         },
     }
 }
