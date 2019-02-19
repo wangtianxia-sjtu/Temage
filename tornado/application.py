8a52d9da05f19cbd8e85263d7a8dea7741aebe25
swc_20180045 <ty0207@sjtu.edu.cn>
2019-02-19 16:39:44 +0800

    add api for text embedding
    add api for text embedding

--- tornado/application.py
+++ tornado/application.py
@@ -1,19 +1,20 @@
 import tornado.web
 import config
-from views import index
+from views import index, embedding
 import sentry_sdk
 from sentry_sdk.integrations.tornado import TornadoIntegration
     
-sentry_sdk.init(
-    dsn="https://ba1972926f5b4e53ad3de8613229170d@sentry.io/1395806",
-    integrations=[TornadoIntegration()]
-)
+# sentry_sdk.init(
+#     dsn="https://ba1972926f5b4e53ad3de8613229170d@sentry.io/1395806",
+#     integrations=[TornadoIntegration()]
+# )
 
 class Application(tornado.web.Application):
     def __init__(self):
         # 路由
         handlers = [
-            (r"/", index.IndexHandler)
+            (r"/", index.IndexHandler),
+            (r"/embedding", embedding.EmbeddingHandler)
         ]
         super(Application, self).__init__(handlers, **config.settings)
         # 加入config里面的settings配置
