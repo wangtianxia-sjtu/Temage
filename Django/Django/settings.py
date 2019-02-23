f55fe67f0d6c82c78014e09541b6ddf60aaf5bae
Unknown <1035254583@qq.com>
2019-02-23 13:55:02 +0800

    update images in doc. change database driver to MyISAM.

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES',foreign_key_checks = 0;",
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES',foreign_key_checks = 0,storage_engine = MyISAM;",
             'charset': 'utf8mb4',
         },
         'TEST_CHARSET': 'utf-8',
